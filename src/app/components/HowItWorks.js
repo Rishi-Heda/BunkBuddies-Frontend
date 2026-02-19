e17eb5d066d0ae472b2edc68d611129a77d7a2d1
AyushK0808 <theofficialayush.kumar@gmail.com>
2026-02-20 02:44:13 +0530

    feat: syon font in how it works along with grid bg

--- src/app/components/HowItWorks.js
+++ src/app/components/HowItWorks.js
@@ -1,32 +1,39 @@
 import React from 'react';
+import { Syne } from 'next/font/google';
+
+const syne = Syne({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+});
 
 const HowItWorks = () => {
   const steps = [
     {
       id: 1,
       title: 'Create a profile',
-      description: 'Create a profile by providing necessary details like name, VIT email ID, registration number, and Rank.',
-      bgColor: 'bg-[#47D19D]', 
+      description:
+        'Create a profile by providing necessary details like name, VIT email ID, registration number, and Rank.',
+      bgColor: 'bg-[#47D19D]',
     },
     {
       id: 2,
       title: 'Find Roommates',
-      description: 'Search for potential roommates or groups on the website and send a request to join them with a short introduction.',
-      bgColor: 'bg-[#BE8EF8]', 
+      description:
+        'Search for potential roommates or groups on the website and send a request to join them with a short introduction.',
+      bgColor: 'bg-[#BE8EF8]',
     },
     {
       id: 3,
       title: 'Initiate Communication',
-      description: 'If you find a potential match, initiate communication through the app. Start with a simple introduction and try to get to know the other person better by asking questions about their lifestyle, habits, and interests',
-      bgColor: 'bg-[#FB5E4C]', 
+      description:
+        'If you find a potential match, initiate communication through the app. Start with a simple introduction and try to get to know the other person better by asking questions about their lifestyle, habits, and interests',
+      bgColor: 'bg-[#FB5E4C]',
     },
   ];
 
   return (
-    <section className="w-full py-12 px-4 md:px-6 lg:px-8 bg-[#fff1e6]"> 
-      {/* Background color similar to the light peach in the image map, but solid */}
+    <section className={`${syne.className} w-full py-12 px-4 md:px-6 lg:px-8`}>
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-        
         {/* Left Side: Title */}
         <div className="flex justify-center md:justify-start">
           <h2 className="text-4xl md:text-5xl font-bold text-black tracking-tight">
@@ -50,7 +57,6 @@ const HowItWorks = () => {
             </div>
           ))}
         </div>
-
       </div>
     </section>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import BackgroundGrid from "./components/BackgroundLines";
 import Landing from "./components/Landing";
-import HowItWorks from "@/components/HowItWorks";
+import HowItWorks from "./components/HowItWorks";
 
 export default function Home() {
   return (
