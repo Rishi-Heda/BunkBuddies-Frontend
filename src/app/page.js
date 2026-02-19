3d5e942db24d294203efe500f49866e21af79bc5
varun4505 <varun452005@gmail.com>
2026-02-20 01:55:41 +0530

    feat: Add HowItWorks section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import BackgroundGrid from "./components/BackgroundLines";
 import Landing from "./components/Landing";
+import HowItWorks from "@/components/HowItWorks";
 
 export default function Home() {
   return (
     <>
     <BackgroundGrid>
       <Landing/>
+      <HowItWorks/>
     </BackgroundGrid>
     </>
   );
