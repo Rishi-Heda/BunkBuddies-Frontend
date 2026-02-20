f084a155351b68eb43480b9bd8914b2539176927
Lakshya Gupta <lakshyaranu@gmail.com>
2026-02-20 12:39:18 +0530

    fix: minor main page

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,11 @@ import HowItWorks from "./components/HowItWorks";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-hidden">
+    <>
       <BackgroundGrid>
         <Landing />
+        <HowItWorks />
       </BackgroundGrid>
-    </div>
+    </>
   );
 }
