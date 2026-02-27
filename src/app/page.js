a50ebe23d34c32f337534e85ee1b72d0161942cb
Vedant <vedant.vidhani@gmail.com>
2026-02-27 23:09:25 +0530

    removed empty lines from page.js for easier pr review

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,12 @@ import Landing from "./components/Landing";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
 
-
 export default function Home() {
   return (
     <>
       <BackgroundGrid>
         <Landing />
         <HowItWorks />
-        
         <Footer />
       </BackgroundGrid>
     </>
